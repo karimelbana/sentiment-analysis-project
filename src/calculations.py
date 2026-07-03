ac00465721c3654183e3839a0099e6ef2628a23c
Karim Elbana <karimhesham.stp@gmail.com>
2026-07-03 12:21:40 +0200

    calculations for testing lecture

---
+++ src/calculations.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def add_two_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b
+
+def divide(a,b):
+    if b == 0:
+        return 'NA'
+    else:
+        return a/b
